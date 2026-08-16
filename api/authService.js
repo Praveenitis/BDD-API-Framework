136f3c02dec38047e784b741cf5ee579c873f05c
Praveen <praveengu19@gmail.com>
2026-08-16 20:36:57 +0530

    Implemented Token manager

--- api/authService.js
+++ api/authService.js
@@ -1,21 +1,37 @@
 const ApiClient = require('./apiClient');
+const TokenManager = require('./tokenManager');
 
 class AuthService {
 
     constructor() {
         this.apiClient = new ApiClient();
+        this.tokenManager = new TokenManager();
         this.baseUrl = 'https://automationintesting.online/api';
     }
 
     async login(credentials) {
 
-        return await this.apiClient.sendRequest(
+        const response = await this.apiClient.sendRequest(
             'POST',
             `${this.baseUrl}/auth/login`,
             {
                 body: credentials
             }
         );
+
+        if (response.status === 200 && response.body.token) {
+            this.tokenManager.setToken(response.body.token);
+        }
+
+        return response;
+    }
+
+    getToken() {
+        return this.tokenManager.getToken();
+    }
+
+    clearToken() {
+        this.tokenManager.clearToken();
     }
 }
 
