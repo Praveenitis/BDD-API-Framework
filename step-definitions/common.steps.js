a4be7fd569f45ba2ebd2872461c7d5f8d8f4708e
Praveen <praveengu19@gmail.com>
2026-08-17 19:06:24 +0530

    create booking feature add

---
+++ step-definitions/common.steps.js
@@ -0,0 +1,14 @@
+const { Given } = require('@cucumber/cucumber');
+
+const AuthService = require('../api/authService');
+
+Given('I am authenticated as an admin', async function () {
+
+    this.authService = new AuthService();
+
+    await this.authService.login({
+        username: 'admin',
+        password: 'password'
+    });
+
+});
