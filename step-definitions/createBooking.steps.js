1175fc759c7de6db2117034a85c63a5ae3006969
Praveen <praveengu19@gmail.com>
2026-08-18 01:25:24 +0530

    Phase 8 completed

--- step-definitions/createBooking.steps.js
+++ step-definitions/createBooking.steps.js
@@ -1,5 +1,8 @@
 const {  When, Then } = require('@cucumber/cucumber');
 const assert = require('assert');
+const TestDataFactory = require('../utils/testDataFactory');
+const RoomService = require('../api/roomService');
+const AvailabilityHelper = require('../utils/availabilityHelper');
 
 
 const BookingService = require('../api/bookingService');
@@ -14,23 +17,47 @@ When('I create a new booking', async function () {
 
     this.bookingService = new BookingService(this.authService);
 
-    const bookingData = {
-        roomid: 1,
-        firstname: 'John',
-        lastname: 'Smith',
-        totalprice: 250,
-        depositpaid: true,
-        bookingdates: {
-            checkin: '2026-09-01',
-            checkout: '2026-09-05'
-        },
-        additionalneeds: 'Breakfast'
-    };
+    const roomService = new RoomService(this.authService);
 
-    this.response = await this.bookingService.createBooking(bookingData);
+    const summaryResponse = await roomService.getBookingSummary(1);
+
+    console.log(
+        'Booking Summary Status:',
+        summaryResponse.status
+    );
+
+    console.log(
+    'Booking Summary Response:',
+    JSON.stringify(summaryResponse.body, null, 2)
+);
+    const roomId = 1;
+
+const availableDates =
+    AvailabilityHelper.findAvailableDates(
+        summaryResponse.body.bookings
+    );
+
+const bookingData =
+    TestDataFactory.generateBookingData(
+        roomId,
+        availableDates.checkin,
+        availableDates.checkout
+    );
+
+
+    this.bookingData = bookingData;
+
+    this.response = await this.bookingService.createBooking(
+        bookingData
+    );
+    if (this.response.body.bookingid) {
+    this.bookingId = this.response.body.bookingid;
+}
+    console.log('Generated Booking ID:', this.bookingId);
 
     console.log('Create Booking Status:', this.response.status);
     console.log('Create Booking Response:', this.response.body);
+
 });
 
 Then('the booking should be created successfully', function () {
@@ -38,4 +65,35 @@ Then('the booking should be created successfully', function () {
     assert.strictEqual(this.response.status, 201);
 
     assert.ok(this.response.body.bookingid);
+});
+
+When('I retrieve the created booking', async function () {
+
+    this.response = await this.bookingService.getBooking(
+        this.bookingId
+    );
+
+    console.log('Get Booking Status:', this.response.status);
+    console.log('Get Booking Response:', this.response.body);
+});
+
+Then('the created booking details should be correct', function () {
+
+    assert.strictEqual(this.response.status, 200);
+
+    assert.strictEqual(
+        this.response.body.firstname,
+        this.bookingData.firstname
+    );
+
+    assert.strictEqual(
+        this.response.body.lastname,
+        this.bookingData.lastname
+    );
+
+    assert.strictEqual(
+        this.response.body.roomid,
+        this.bookingData.roomid
+    );
+
 });
