08c7bfc002f03af6820d62e5251a43f7736edb67
Praveen <praveengu19@gmail.com>
2026-08-18 01:24:47 +0530

    Phase 8 completed

--- step-definitions/auth.steps.js
+++ step-definitions/auth.steps.js
@@ -17,14 +17,18 @@ Given('valid admin credentials', function () {
 
 });
 
-When('I send an authentication request', async function () {
+When(
+    'I send an authentication request',
+    { timeout: 15000 },
+    async function () {
 
-    response = await authService.login(credentials);
+        response = await authService.login(credentials);
 
-    console.log('Status:', response.status);
-    console.log('Response:', response.body);
+        console.log('Status:', response.status);
+        console.log('Response:', response.body);
 
-});
+    }
+);
 
 Then('an authentication token should be generated', function () {
 
